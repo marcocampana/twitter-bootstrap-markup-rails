d06fc3340095d1629e6ebee5354ed5cda229ee0b
Luca Spiller <luca@stackednotion.com>
2012-03-29 15:02:23 +0100

    Add tooltip option to check boxes

--- lib/twitter-bootstrap-markup-rails/components/form/check_box.rb
+++ lib/twitter-bootstrap-markup-rails/components/form/check_box.rb
@@ -14,10 +14,15 @@ module Twitter::Bootstrap::Markup::Rails::Components
         html = ''
         html << element_html
         html << options[:label]
+        html << tooltip(options[:tooltip]) unless options[:tooltip].blank?
         html.html_safe
       end
       super
     end
 
+    def tooltip(text)
+      question_sign_tag = content_tag :i, nil, :class => 'icon-question-sign'
+      content_tag :a, question_sign_tag, :class => 'help', :rel => 'tooltip', :href => '#', :title => text
+    end
   end
 end
